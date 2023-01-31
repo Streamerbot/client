dfa736a69fe9e2b00807733e7a4d6cfc77403ed9
Whipstickgostop <8366326+Whipstickgostop@users.noreply.github.com>
2023-01-31 13:48:23 -0500

    manual favicon config

--- apps/docs/nuxt.config.ts
+++ apps/docs/nuxt.config.ts
@@ -2,6 +2,9 @@ export default defineNuxtConfig({
   extends: '@nuxt-themes/docus',
   app: {
     baseURL: '/client/',
+    head: {
+      link: [{ rel: 'icon', type: 'image/png', href: '/client/favicon.ico' }],
+    },
   },
   colorMode: {
     preference: 'dark',
