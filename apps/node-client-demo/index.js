28d5a7f37178cc1a8a116cb4145b5772d2bad4bf
Whipstickgostop <8366326+Whipstickgostop@users.noreply.github.com>
2023-02-04 15:39:09 -0500

    test actions response

--- apps/node-client-demo/index.js
+++ apps/node-client-demo/index.js
@@ -2,5 +2,9 @@ import { StreamerbotClient } from '@streamerbot/client';
 
 const client = new StreamerbotClient({ host: 'aphex', immediate: false });
 await client.connect();
+
 const info = await client.getInfo();
 console.log(`Connected to Streamer.bot`, info);
+
+const actions = await client.getActions();
+console.log('Actions received:', actions);
