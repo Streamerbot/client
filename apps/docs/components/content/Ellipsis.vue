f9f2bed5dd4177c0ac981ced02eac706d6a7b7d4
Ik <113038466+Ik1497@users.noreply.github.com>
2023-05-12 17:38:53 +0200

    Fix horizontal scrollbar issue and fix height on the ellipsis component

--- apps/docs/components/content/Ellipsis.vue
+++ apps/docs/components/content/Ellipsis.vue
@@ -13,8 +13,8 @@
   top: 0;
   bottom: 0;
   z-index: -1;
-  width: 100vw;
-  height: 80%;
+  width: 100%;
+  height: 100%;
   background: #0c0c0d;
   background-image: radial-gradient(ellipse at 0% 0%, rgb(26, 214, 255, 0.01), rgb(26, 214, 255, 0.001) 60%),
     radial-gradient(ellipse at 20% 10%, rgb(26, 214, 255, 0.01), rgb(26, 214, 255, 0.001) 40%),
