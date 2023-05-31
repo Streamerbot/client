4f16914cf1a17f2cb890d9b1ceb5e4667eb26fdd
Whipstickgostop <8366326+Whipstickgostop@users.noreply.github.com>
2023-05-31 11:13:00 -0400

    add source field

--- packages/client/src/ws/types/StreamerbotInfoTypes.ts
+++ packages/client/src/ws/types/StreamerbotInfoTypes.ts
@@ -1,6 +1,31 @@
 export type StreamerbotInfo = {
+  /**
+   * The ID of the connected Streamer.bot instance
+   * @example 'b63152c5-5bfe-4751-8644-579f7fb4a682'
+   */
   instanceId: string;
+
+  /**
+   * The name of the connected Streamer.bot instance
+   * @example 'Streamer.bot'
+   */
   name: string;
+
+  /**
+   * The operating system of the connected Streamer.bot instance
+   * @example 'windows'
+   */
   os: 'windows' | 'linux' | 'macosx' | string;
+
+  /**
+   * The version of the connected Streamer.bot instance
+   * @example '0.1.21'
+   */
   version: string;
-}
+
+  /**
+   * The source of the Streamer.bot connection
+   * @description Requires Streamer.bot v0.1.21 or higher
+   */
+  source?: 'websocketServer' | 'streamDeckServer' | 'httpServer' | string;
+};
